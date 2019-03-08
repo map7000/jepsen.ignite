002545c9fb5da52ee6627739e3b221ef500d783f
Mikhail Filatov <misha@inspirational.ru>
2019-03-08 13:09:09 +0300

    Switch to Alpine

--- src/jepsen/ignite/support.clj
+++ src/jepsen/ignite/support.clj
@@ -1,32 +1,31 @@
 (ns jepsen.ignite.support
-    (:require [clojure.tools.logging :refer :all]
-              [clojure.string :as str]
-              [clojure.java.io :as io]
-              [jepsen
-               [cli :as cli]
-               [tests :as tests]
-               [control :as c]
-               [core :as jepsen]
-               [db :as db]
-               [util :as ju]
-               [client :as client]
-               [checker :as checker]
-               [nemesis :as nemesis]
-               [generator :as gen]
-               [independent :as independent]]
-              [jepsen.checker.timeline :as timeline]
-              [knossos.model :as model]
-              [jepsen.control.util :as cu])
-    (:import (clojure.lang ExceptionInfo)
-             (org.apache.ignite Ignition IgniteCache)
-             (org.apache.ignite.cache CacheMode CacheAtomicityMode CacheWriteSynchronizationMode)
-             (org.apache.ignite.configuration CacheConfiguration)
-             (org.apache.ignite.transactions TransactionConcurrency TransactionIsolation)
-             (java.lang Integer)
-             (java.io File FileNotFoundException)))
+  (:require [clojure.tools.logging :refer :all]
+            [clojure.string :as str]
+            [clojure.java.io :as io]
+            [jepsen
+             [cli :as cli]
+             [tests :as tests]
+             [control :as c]
+             [core :as jepsen]
+             [db :as db]
+             [util :as ju]
+             [client :as client]
+             [checker :as checker]
+             [nemesis :as nemesis]
+             [generator :as gen]
+             [independent :as independent]]
+            [jepsen.checker.timeline :as timeline]
+            [knossos.model :as model]
+            [jepsen.control.util :as cu])
+  (:import (clojure.lang ExceptionInfo)
+           (org.apache.ignite Ignition IgniteCache)
+           (org.apache.ignite.cache CacheMode CacheAtomicityMode CacheWriteSynchronizationMode)
+           (org.apache.ignite.configuration CacheConfiguration)
+           (org.apache.ignite.transactions TransactionConcurrency TransactionIsolation)
+           (java.lang Integer)
+           (java.io File FileNotFoundException)))
 
-;(def dir "/gridgain/sas/jepsen.ignite")
-(def dir "/tmp/jepsen.ignite")
+(def dir "/jepsen.ignite")
 (def logfile (str dir "/ignite.log"))
 (def pidfile (str dir "/ignite.pid"))
 
@@ -39,7 +38,7 @@
   [node test]
   (info node "killing stalled nodes")
   (ju/meh
-   (c/exec :pkill :-9 :-f "org.apache.ignite.startup.cmdline.CommandLineStartup")))
+    (c/exec :pkill :-9 :-f "org.apache.ignite.startup.cmdline.CommandLineStartup")))
 
 (defn awaitStartedGrid
   "Waiting for started grid"
@@ -48,9 +47,9 @@
   (io/delete-file (localNodeLogFileName node) true)
   (c/download (str dir "/ignite.log") (localNodeLogFileName node))
   (while
-   (not (str/includes? (slurp (localNodeLogFileName node)) "servers=5"))
-   (Thread/sleep 5000)
-   (c/download (str dir "/ignite.log") (localNodeLogFileName node))))
+    (not (str/includes? (slurp (localNodeLogFileName node)) "servers=5"))
+    (Thread/sleep 5000)
+    (c/download (str dir "/ignite.log") (localNodeLogFileName node))))
 
 
 (defn install!
@@ -61,9 +60,9 @@
   (c/exec :touch (str dir "/ignite.log"))
   (c/cd (str dir)
         (info node (str "Copy Ignite-" version))
-        (c/upload (str "/gridgain/sas/apache-ignite-" version "-bin.zip") dir)
-        (info node (str "Unzip Ignite-" version))
-        (c/exec :unzip :-qqo (str "apache-ignite-" version "-bin.zip"))))
+        (c/upload (str "./resources/ignite.tar.gz") dir)
+        (info node (str "Untar Ignite-" version))
+        (c/exec :tar :-zxf :ignite.tar.gz)))
 
 (defn configure!
   "Uploads configuration files to the given node."
@@ -83,8 +82,8 @@
   [node]
   (info node "Killing Ignite")
   (ju/meh
-   (c/exec
-    (c/lit "ps -ef | grep ignite.sh | grep -v grep | awk '{print $2}'| xargs kill -9"))))
+    (c/exec
+      (c/lit "ps -ef | grep ignite.sh | grep -v grep | awk '{print $2}'| xargs kill -9"))))
 
 (defn clean!
   "Wipes data."
@@ -97,27 +96,27 @@
   "Ignite for a particular version."
   [version]
   (reify
-   db/DB
-   (setup! [_ test node]
-           (killStalledNodes node test)
-           (install! node version)
-           (configure! node)
-           (jepsen/synchronize test)
-           (start! node)
-           (awaitStartedGrid node test)
-           (jepsen/synchronize test))
-   (teardown! [_ test node]
-              (stop! node)
-              (clean! node))
+    db/DB
+    (setup! [_ test node]
+      (killStalledNodes node test)
+      (install! node version)
+      (configure! node)
+      (jepsen/synchronize test)
+      (start! node)
+      (awaitStartedGrid node test)
+      (jepsen/synchronize test))
+    (teardown! [_ test node]
+      (stop! node)
+      (clean! node))
 
-   db/LogFiles
-   (log-files [_ test node]
-              [logfile])))
+    db/LogFiles
+    (log-files [_ test node]
+      [logfile])))
 
 (defn killer
   "Kills random node"
   []
   (nemesis/node-start-stopper
-   rand-nth
-   (fn start [test node] (stop! node))
-   (fn stop [test node] (start! node))))
+    rand-nth
+    (fn start [test node] (stop! node))
+    (fn stop [test node] (start! node))))
